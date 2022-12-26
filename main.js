7d8fe81d488bbd79ceaf9b988a0343794e1bf0de
blackjoker14 <86548199+blackjoker14@users.noreply.github.com>
2022-12-26 15:19:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+video = "";
+status = "";
+object = [];
+
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function mobelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    video.loop( );
+    video.speed(1);
+    video.volumn(0);
+}
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', mobelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+   }
